8141c6de215c87ea1d15bc4aa678f1138774ee1d
PeRoBr <peter.brotherhood@gmail.com>
2017-02-09 18:35:56 +1100

    This is my completed week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix allows the user to enter a matrix using the set "sub function", 
+## verify the content of the matrix with the get "sub function", set the value of an 
+## inverse matrix with the set "sub function" and retrieve the value of the inverse
+## matrix with the get_solve "sub function"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        sol <- NULL          
+        set <- function(y) {  
+                x <<- y
+                sol <<- NULL
+        }
+        get <- function() x
+        set_solve <- function(solve) sol <<- solve
+        get_solve <- function() sol
+        list(set = set, 
+             get = get, 
+             set_solve = set_solve, 
+             get_solve = get_solve)
 }
 
 
-## Write a short comment describing this function
+## Takes as input the matrices stored by the functions above. Looks to see if a non NULL
+## matrix is stored as the inverse, and returns it if it exists, first giving the   
+## message "getting cached data", otherwise it calculates the inverse from the matrix
+## set in makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
+        sol <- x$get_solve()
+        if(!is.null(sol)) {
+                message("getting cached data")
+                return(sol)
+        }
         ## Return a matrix that is the inverse of 'x'
+        data <- x$get()
+        sol <- solve(data, ...)
+        x$set_solve(sol)
+        sol
 }
+
+## this completes the assignment.
